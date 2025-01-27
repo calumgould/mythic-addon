5af25eca19869feaf90fb2258f50e2a8da72500f
calumgould <calumgould26@gmail.com>
2025-01-27 17:40:58 +0000

    Try adding damage calculator macro

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,21 +1,28 @@
+import fs from 'fs'
+import path from 'path'
+
 const createMacros = async () => {
     console.log("Creating macros")
+    const damageCalculatorCommand = fs.readFileSync(path.join(__dirname, '../scripts/macros/damageCalculator.js'), 'utf-8');
 
-    const testMacro = await Macro.create({
-        name: 'Mythic addon test macro',
+    console.log("Command", damageCalculatorCommand)
+
+    const damageCalculatorMacro = await Macro.create({
+        name: 'Damage Calculator Macro',
         type: 'script',
-        command: "console.log('This is a Mythic test macro');",
+        command: damageCalculatorCommand,
         img: "icons/svg/d20.svg",
-        permission: { default: 0 }
+        permission: { default: 0 },
+        author: 'Calum Gould'
     })
 
-    console.log("Macro created", testMacro)
+    console.log("Macro created", damageCalculatorMacro)
 
     const compendium = await game.packs.get('mythic-addon.mythic-addon-macros');
 
     console.log("Compendium", compendium)
 
-    await compendium.importEntity(testMacro)
+    await compendium.importEntity(damageCalculatorMacro)
 }
 
 Hooks.on("init", async () => {
