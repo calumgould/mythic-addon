1321a5027dbc97c917351a4cce03a3dbd89c64d8
calumgould <calumgould26@gmail.com>
2025-01-27 18:44:59 +0000

    Add macro pack manually

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 
 This is an addon module for the Mythic system, which can be found here https://github.com/AugmenTab/mythic
 
+### Current Features
+
+#### Damage Calculator
+
+Macro that reads the latest attack from the chat messages and calculates the damage that the selected token or current character takes.
+
+This is still early in development and is likely missing a lot of interactions that should be specifically handled.
+
+So far the logic covers the basics, plus:
+- Ignore certain hits (e.g. evading)
+- Headshots (weapons with headshot special rule)
+- Burst fire (multiple damage instances on one hit)
+- Shields (including some weapons adding their pierce to shield damage)
+- Areas of target being in cover
+- Extra pierce (e.g. from a charge attack)
+- Damage multiplier (e.g. from kill radius on a grenade)
+
+Also includes:
+- Whisper result
+  - This option will send the result as a whisper to the player who triggered the macro instead of publicly.
--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,30 +1,3 @@
-const createDamageCalculatorMacro = async () => {
-    console.log("Creating macros")
-
-    const existingDamageCalculatorMacro = game.macros.find((macro: any) => macro.name === 'Damage Calculator Macro')
-
-    console.log("Existing macro", existingDamageCalculatorMacro)
-
-    const damageCalculatorMacro = await Macro.create({
-        name: 'Damage Calculator Macro',
-        type: 'script',
-        command: 'console.log("Damage Calculator Macro");',
-        img: "icons/svg/d20.svg",
-        permission: { default: 0 },
-        author: 'Calum Gould'
-    })
-
-    console.log("Macro created", damageCalculatorMacro)
-
-    const compendium = await game.packs.get('mythic-addon.mythic-addon-macros');
-
-    console.log("Compendium", compendium)
-
-    await compendium.importEntity(damageCalculatorMacro)
-}
-
-Hooks.on("init", async (data: any) => {
-    console.log("Init data", data)
-    await createDamageCalculatorMacro()
+Hooks.on("ready", () => {
     console.log("Mythic-addon module loaded")
 });
