cfb9028fcdc89283eb2a111e21cde1a8936fbf7f
calumgould <calumgould26@gmail.com>
2025-01-27 15:26:20 +0000

    Remove foundry-vtt-types

---
+++ foundry.d.ts
@@ -0,0 +1,6 @@
+// I'd must rather use https://github.com/League-of-Foundry-Developers/foundry-vtt-types instead but they don't have a working v11 release
+// If mythic eventually supported foundry v12 then could swap to the above instead
+declare const Hooks: any;
+declare const game: any;
+declare const canvas: any;
+declare const ChatMessage: any
