c5cb1c3cb73f27fd7b8f4ab1ca9314af6a5398ce
calumgould <calumgould26@gmail.com>
2025-01-27 17:53:49 +0000

    Macro testing

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,16 +1,14 @@
-import fs from 'fs'
-import path from 'path'
-
-const createMacros = async () => {
+const createDamageCalculatorMacro = async () => {
     console.log("Creating macros")
-    const damageCalculatorCommand = fs.readFileSync(path.join(__dirname, '../scripts/macros/damageCalculator.js'), 'utf-8');
 
-    console.log("Command", damageCalculatorCommand)
+    const existingDamageCalculatorMacro = game.macros.find((macro: any) => macro.name === 'Damage Calculator Macro')
+
+    console.log("Existing macro", existingDamageCalculatorMacro)
 
     const damageCalculatorMacro = await Macro.create({
         name: 'Damage Calculator Macro',
         type: 'script',
-        command: damageCalculatorCommand,
+        command: 'console.log("Damage Calculator Macro");',
         img: "icons/svg/d20.svg",
         permission: { default: 0 },
         author: 'Calum Gould'
@@ -25,7 +23,8 @@ const createMacros = async () => {
     await compendium.importEntity(damageCalculatorMacro)
 }
 
-Hooks.on("init", async () => {
-    await createMacros()
+Hooks.on("init", async (data: any) => {
+    console.log("Init data", data)
+    await createDamageCalculatorMacro()
     console.log("Mythic-addon module loaded")
 });
